76d2d5120623dd028bc7a1d8c43dabe75388460a
Vladislav Pavlov <atobytes@mail.ru>
2023-02-10 02:23:26 +0300

    code reformat

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BonusService service = new BonusService();
 
         int expected = 150;
-        long actual = service.calculate(1000, false  );
+        long actual = service.calculate(1000, false);
         System.out.println("1. " + expected + "== ? ==" + actual);
 
     }
